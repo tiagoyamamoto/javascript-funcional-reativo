adc81c0002e43300bf645b4836907be777e86b4b
Tiago Yamamoto <japa@rede5.com.br>
2022-02-24 23:33:12 -0300

    Separando Linhas

--- projeto_1/funcoes.js
+++ projeto_1/funcoes.js
@@ -17,7 +17,24 @@ function elementosTerminadosCom(array, padrao) {
     return array.filter(el => el.endsWith(padrao))
 }
 
+function lerArquivo(caminho) {
+    return new Promise((resolve, reject) => {
+        try {
+            const conteudo = fs.readFileSync(caminho, { enconding: 'utf-8' })
+            resolve(conteudo.toString())
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+function lerArquivos(caminhos) {
+     return Promise.all(caminhos.map(caminho => lerArquivo(caminho)))
+}
+
 module.exports = {
     lerDiretorio,
+    lerArquivo,
+    lerArquivos,
     elementosTerminadosCom
 }
--- projeto_1/index.js
+++ projeto_1/index.js
@@ -6,4 +6,7 @@ const caminho = path.join(__dirname, '..', 'dados', 'legendas')
 fn.lerDiretorio(caminho)
     // .then
     .then(arquivos => fn.elementosTerminadosCom(arquivos, '.srt'))
+    .then(arquivosSRT => fn.lerArquivos(arquivosSRT))
+    .then(conteudos => conteudos.join(''))
+    .then(todoConteudo => todoConteudo.split('\n'))
     .then(console.log)
