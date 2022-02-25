05f70bd59dc6f418e318dc9c5e68ed4e529d81d1
Yamamoto <yamamoto@inspiron>
2022-02-24 22:52:09 -0300

    Filtrando Arquivos por Extensão

--- projeto_1/index.js
+++ projeto_1/index.js
@@ -5,4 +5,5 @@ const caminho = path.join(__dirname, '..', 'dados', 'legendas')
 
 fn.lerDiretorio(caminho)
     // .then
+    .then(arquivos => fn.elementosTerminadosCom(arquivos, '.srt'))
     .then(console.log)
