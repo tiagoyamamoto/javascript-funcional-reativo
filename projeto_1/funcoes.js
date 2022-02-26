2076b90a9b9179b6e1e73c28158ddc5b68e31283
Tiago Yamamoto <japa@rede5.com.br>
2022-02-26 15:59:33 -0300

    Ordenando Palavras

--- projeto_1/funcoes.js
+++ projeto_1/funcoes.js
@@ -87,6 +87,15 @@ function agruparElementos(palavras) {
     }, {}))
 }
 
+
+function ordernarPorAtribNumerico(attr, ordem = 'asc') {
+    return function (array) {
+        const asc = (o1, o2) => o1[attr] - o2[attr]
+        const desc = (o1, o2) => o2[attr] - o1[attr]
+        return [...array].sort(ordem === 'asc' ? asc : desc)
+    }
+}
+
 module.exports = {
     lerDiretorio,
     lerArquivo,
@@ -98,5 +107,6 @@ module.exports = {
     removerSimbolos,
     mesclarElementos,
     separarTextoPor,
-    agruparElementos
+    agruparElementos,
+    ordernarPorAtribNumerico
 }
